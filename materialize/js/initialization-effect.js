080f35b5f494eabf5241aac283fe03de729d155a
BrunaPires <b.serip@gmail.com>
2021-02-07 08:18:54 -0300

    Commit

---
+++ materialize/js/initialization-effect.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // paralax effect
+    $('.parallax').parallax();
+
+    // sidenav effect
+    $('.sidenav').sidenav();
+
+    // slider effect
+    $('.slider').slider();
+});
+
